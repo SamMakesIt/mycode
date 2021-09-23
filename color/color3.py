988bada4078212f98d99817a675f73ce9900942c
sampaladino <sampaladino88@gmail.com>
2021-09-23 17:46:25 +0000

    Graph

---
+++ color/color3.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Alta3 Research || Author RZFeeser@alta3.com
+Learning how to use functions"""
+
+## Installs the crayons package.
+## python3 -m pip install crayons
+## import statements ALWAYS go up top
+import crayons
+
+def main():
+    """Runtime code. Always indent a function"""
+
+    # Red White and Blue text
+    print('S', (crayons.red('A'), crayons.blue('M')))
+
+    
+
+# we must call our main function or our code will not run!
+main()
+
